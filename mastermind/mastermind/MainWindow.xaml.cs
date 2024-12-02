546a70602af6bb64a0effe2f352b260bb782ca32
ThibaultLesecquePXL <thibault.lesecque@student.pxl.be>
2024-12-02 14:00:57 +0100

    Mastermind-PE2-01

---
+++ mastermind/mastermind/MainWindow.xaml.cs
@@ -0,0 +1,265 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace mastermind
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random rnd = new Random();
+
+        SolidColorBrush[] colorSelection = [Brushes.Red, Brushes.Blue, Brushes.Green, Brushes.White, Brushes.Yellow, Brushes.Orange];
+        string[] colorSelectionString = ["Red", "Blue", "Green", "White", "Yellow", "Orange"];
+        int[] colorsRandom = new int[4];
+
+        int attempts = 0;
+        int score = 100;
+        bool isPlaying = true;
+
+        DispatcherTimer timer = new DispatcherTimer();
+        DateTime startTime;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ComboBoxItemsInit();
+            NewGame();
+        }
+
+        private void PickColors()
+        {
+            for (int i = 0; i < colorsRandom.Length; i++)
+            {
+                colorsRandom[i] = rnd.Next(1, 6);
+            }
+
+            debugTextBox.Text = $"Oplossing: ({colorSelectionString[colorsRandom[0]]}, {colorSelectionString[colorsRandom[1]]}, {colorSelectionString[colorsRandom[2]]}, {colorSelectionString[colorsRandom[3]]})";
+        }
+
+        private void ComboBoxItemsInit()
+        {
+            color1ComboBox.ItemsSource = colorSelectionString;
+            color2ComboBox.ItemsSource = colorSelectionString;
+            color3ComboBox.ItemsSource = colorSelectionString;
+            color4ComboBox.ItemsSource = colorSelectionString;
+        }
+
+        private void ComboBox_SelectionChanged (object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = sender as ComboBox;
+
+            if (comboBox.Name == "color1ComboBox")
+            {
+                color1Ellipse.Fill = colorSelection[comboBox.SelectedIndex];
+            }
+            else if (comboBox.Name == "color2ComboBox")
+            {
+                color2Ellipse.Fill = colorSelection[comboBox.SelectedIndex];
+            }
+            else if (comboBox.Name == "color3ComboBox")
+            {
+                color3Ellipse.Fill = colorSelection[comboBox.SelectedIndex];
+            }
+            else if (comboBox.Name == "color4ComboBox")
+            {
+                color4Ellipse.Fill = colorSelection[comboBox.SelectedIndex];
+            }
+        }
+
+        private void CheckButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (isPlaying)
+            {
+                CheckComboBox(color1Ellipse, color1ComboBox, 0);
+                CheckComboBox(color2Ellipse, color2ComboBox, 1);
+                CheckComboBox(color3Ellipse, color3ComboBox, 2);
+                CheckComboBox(color4Ellipse, color4ComboBox, 3);
+                UpdateAttempts();
+                StartCountdown();
+            }
+            
+        }
+
+        private void CheckComboBox(Ellipse elipse, ComboBox combobox, int number)
+        {
+            elipse.Stroke = Brushes.Black;
+            elipse.StrokeThickness = 1;
+
+            if (combobox.SelectedIndex == colorsRandom[number])
+            {
+                elipse.Stroke = Brushes.DarkRed;
+                elipse.StrokeThickness = 5;
+            }
+            else if (colorsRandom.Contains(combobox.SelectedIndex))
+            {
+                elipse.Stroke = Brushes.Wheat;
+                elipse.StrokeThickness = 5;
+                score -= 1;
+            }
+            else
+            {
+                score -= 2;
+            }
+
+            scoreLabel.Content = $"Current score: {score}";
+        }
+
+        private void UpdateAttempts()
+        {
+            attempts += 1;
+            this.Title = $"Poging {attempts}";
+
+            if (attempts >= 10)
+            {
+                isPlaying = false;
+                EndGame(false);
+            }
+        }
+
+        private void mastermind_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.F12)
+            {
+                ToggleDebug();
+            }
+        }
+
+        /// <summary>
+        /// De ToggleDebug functie togglet tussen het wel en niet
+        /// tonen van een textbox met daarin de willekeurig gegenereerde oplossing
+        /// </summary>
+        private void ToggleDebug()
+        {
+            if (debugTextBox.Visibility == Visibility.Hidden)
+            {
+                debugTextBox.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                debugTextBox.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            TimeSpan elapsed = DateTime.Now - startTime;
+            timerLabel.Content = elapsed.ToString(@"ss");
+
+            if (elapsed.TotalSeconds > 11)
+            {
+                StopCountdown();
+            }
+        }
+
+        /// <summary>
+        /// De StartCountdown functie start een secondenteller die
+        /// zal tellen vanaf 0 en door zal lopen tot 10
+        /// </summary>
+        private void StartCountdown()
+        {
+            if (isPlaying)
+            {
+                timerLabel.Content = "00";
+                timer.Tick += Timer_Tick;
+                timer.Interval = new TimeSpan(0, 0, 1);
+                timer.Start();
+                startTime = DateTime.Now;
+            }
+        }
+
+        /// <summary>
+        /// <para>De StopCountdown functie stopt de secondenteller en
+        /// zal deze opnieuw laten starten.</para>
+        /// <para> Na het stoppen van de voorgaande teller zal ook
+        /// de UpdateAttempts functie aangeroepen worden.</para>
+        /// </summary>
+        private void StopCountdown()
+        {
+            timer.Stop();
+            UpdateAttempts();
+            StartCountdown();
+        }
+
+        private void NewGame()
+        {
+            attempts = 0;
+            isPlaying = true;
+
+            PickColors();
+            UpdateAttempts();
+            StartCountdown();
+
+            score = 100;
+            scoreLabel.Content = $"Current score: {score}";
+        }
+
+        private void EndGame(bool hasWon)
+        {
+            isPlaying = false;
+            timer.Stop();
+
+            if (hasWon)
+            {
+                MessageBoxResult Result = MessageBox.Show($"Code is gekraakt in {attempts} pogingen. Wil je nog eens?", "WINNER", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (Result == MessageBoxResult.Yes)
+                {
+                    NewGame();
+                }
+                else if (Result == MessageBoxResult.No)
+                {
+                    this.Close();
+                }
+            }
+            else if (!hasWon)
+            {
+                MessageBoxResult Result = MessageBox.Show($"You failed! De correcte code was {colorSelectionString[colorsRandom[0]]}, {colorSelectionString[colorsRandom[1]]}, {colorSelectionString[colorsRandom[2]]}, {colorSelectionString[colorsRandom[3]]}. Nog eens proberen?", "FAILED", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (Result == MessageBoxResult.Yes)
+                {
+                    NewGame();
+                }
+                else if (Result == MessageBoxResult.No)
+                {
+                    this.Close();
+                }
+            }
+        }
+
+        private void Mastermind_Close(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            var result = MessageBox.Show("Wilt u het spel vroegtijdig beëindigen?", $"Poging {attempts}/10", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+
+            if (result == MessageBoxResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void MnuNew_Click(object sender, System.EventArgs e)
+        {
+            NewGame();
+        }
+
+        private void MnuHighscores_Click(object sender, System.EventArgs e)
+        {
+            MessageBox.Show("No highscores", "Mastermind highscores", MessageBoxButton.OK);
+        }
+
+        private void MnuExit_Click(object sender, System.EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
